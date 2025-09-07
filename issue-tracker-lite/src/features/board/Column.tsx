b84978e5b3b8ba21d67c147bf7252343c33aee9b
y-k17 <a.syadukrisnan2002@gmail.com>
2025-09-08 00:00:32 +0530

    Implemented functions in column

--- issue-tracker-lite/src/features/board/Column.tsx
+++ issue-tracker-lite/src/features/board/Column.tsx
@@ -1,16 +1,32 @@
 import { IssueCard} from '../issues/Issues';
+import type { Issue } from '../issues/types';
 
 type ColumnProps = {
-  title: string;
+  title: Issue["status"];
+  issues: Issue[];
+  onMove: (id: string, newStatus: Issue["status"]) => void;
+  onDelete: (id: string) => void;
 };
 
-export function Column({ title }: ColumnProps) {
+export function Column({ title, issues, onMove, onDelete }: ColumnProps) {
   return (
-    <div style={{ flex: 1, padding: "8px", background: "#777777ff", borderRadius: "8px" }}>
+    <div
+      style={{
+        flex: 1,
+        padding: "8px",
+        background: "#777777ff",
+        borderRadius: "8px",
+      }}
+    >
       <h2 style={{ textAlign: "center" }}>{title}</h2>
-      {/* Hardcoded cards for now */}
-      <IssueCard title="Setup project skeleton" assignedTo="Alice" priority="low" />
-      <IssueCard title="Add routing" assignedTo="Bob" priority="high" />
+      {issues.map((issue) => (
+        <IssueCard
+          key={issue.id}
+          issue={issue}
+          onMove={onMove}
+          onDelete={onDelete}
+        />
+      ))}
     </div>
   );
 }
