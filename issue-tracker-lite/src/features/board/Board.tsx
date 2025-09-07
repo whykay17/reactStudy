9aaf4fc19b7614a9e38f787df1ce5adfc57758ee
y-k17 <a.syadukrisnan2002@gmail.com>
2025-09-08 00:00:55 +0530

    Added move, delete and add issue buttons in board

--- issue-tracker-lite/src/features/board/Board.tsx
+++ issue-tracker-lite/src/features/board/Board.tsx
@@ -1,7 +1,47 @@
 import { Column } from "./Column";
+import type { Issue } from "../issues/types";
+import { useState } from "react";
 
 export function Board() {
-  return (
+  const [issues, setIssues] = useState<Issue[]>([
+    {
+      id: crypto.randomUUID(),
+      title: "Setup project skeleton",
+      assignee: "Alice",
+      priority: "low",
+      status: "backlog",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Add routing",
+      assignee: "Bob",
+      priority: "high",
+      status: "inprogress",
+    },
+  ]);
+
+  function addIssue(title: string, assignee: string, priority: Issue['priority']) {
+    const newIssue: Issue = {
+      id: crypto.randomUUID(),
+      title,
+      assignee,
+      priority,
+      status: 'backlog'
+    };
+    setIssues((prev) => [newIssue, ...prev]);
+  }
+
+  function moveIssue(id: string, status:Issue['status']) {
+    setIssues((prev) =>
+      prev.map((issue) => (issue.id === id ? { ...issue, status: status } : issue)));
+  }
+
+  function deleteIssue(id: string) {
+    setIssues((prev) => prev.filter((i) => i.id !== id));
+  }
+
+  return (<>
+    < button onClick={() => addIssue("New Issue", "Charlie", "med")}>Add Random Issue</button>
     <div
       style={{
         display: "flex",
@@ -11,9 +51,16 @@ export function Board() {
         minHeight: "100vh",
       }}
     >
-      <Column title="Backlog" />
-      <Column title="In Progress" />
-      <Column title="Done" />
+      {['backlog', 'inprogress', 'done'].map((col) => (
+        <Column 
+          key={col}
+          title= {col as Issue['status']}
+          issues = {issues.filter((i)=> i.status === col)}
+          onMove={moveIssue}
+          onDelete={deleteIssue}
+        />
+      ))}
     </div>
-  );
+  </>
+  )
 }
