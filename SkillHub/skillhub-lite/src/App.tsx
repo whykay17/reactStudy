588e4e1994189ab7e8e65db4866f04247978bafd
y-k17 <a.syadukrisnan2002@gmail.com>
2025-10-04 23:40:29 +0530

    Skillhub init with tailwind

---
+++ SkillHub/skillhub-lite/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold text-blue-600">
+        Hello
+       </div>
+    </>
+  )
+}
+
+export default App
