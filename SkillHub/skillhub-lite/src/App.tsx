f7139607746361899a6aac0edc1dd34b12e23fbc
y-k17 <a.syadukrisnan2002@gmail.com>
2025-10-04 23:54:55 +0530

    Routing set up

--- SkillHub/skillhub-lite/src/App.tsx
+++ SkillHub/skillhub-lite/src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const GigListPage = () => <h1 className='text-xl p-4'>Gig List Page (/)</h1>;
+const LoginPage = () => <h1 className="text-xl p-4">Login Page (/login)</h1>;
+const DashboardPage = () => <h1 className="text-xl p-4">Dashboard Page (/dashboard)</h1>;
 
 function App() {
 
   return (
     <>
-      <div className="text-3xl font-bold text-blue-600">
-        Hello
-       </div>
+      <header className="bg-blue-600 text-white p-4 shadow-lg">
+        <h1 className="text-2xl font-bold">SkillHub Lite</h1>
+      </header>
+      <main className="p-8">
+        <Routes>
+          <Route path="/" element = {GigListPage()} />
+          <Route path="/login" element = {LoginPage()} />
+          <Route path="/dashboard" element = {DashboardPage()} />
+        </Routes>
+      </main>
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </>
   )
 }
--- SkillHub/skillhub-lite/src/main.tsx
+++ SkillHub/skillhub-lite/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
